332d20dd2e6d48117c8b3789850d1f50756f2b94
Welinton <welinton@realizati.com.br>
2020-08-20 20:28:30 -0300

    Criado atributos da instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=33):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Subtil')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar()) # Passagem implicita de p
+    print(p.nome)
+    p.nome = 'Welinton'
+    print(p.nome)
