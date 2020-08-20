880eb5782858bac80794936a7a6f37199ae0f9fb
Welinton <welinton@realizati.com.br>
2020-08-20 20:33:07 -0300

    Criado atributo complexo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=33):
+    def __init__(self, *filhos, nome=None, idade=33):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Subtil')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar()) # Passagem implicita de p
-    print(p.nome)
-    p.nome = 'Welinton'
-    print(p.nome)
+    gabriel = Pessoa(nome='Gabriel')
+    subtil = Pessoa(gabriel, nome='Subtil')
+    print(id(subtil))
+    print(subtil.cumprimentar()) # Passagem implicita de p
+    print(subtil.nome)
+    print(subtil.idade)
+    for filho in subtil.filhos:
+        print(filho.nome)
